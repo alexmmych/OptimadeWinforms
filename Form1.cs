ad651c49e9cd1d7c4cb9fd3f84c3b39a58e4c206
HighwayToHeaven <alexmmych@gmail.com>
2021-01-25 17:00:57 +0100

    Optimade but made with Winforms and C#

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Optimade
+{
+    public partial class Optimade : Form
+    {
+
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("www.google.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+
+        public Optimade()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            InitializeChromium();
+        }
+
+
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+            );
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+
+            dragCursorPoint = Cursor.Position;
+
+            Console.WriteLine("Cursor Y:" + " " + (dragCursorPoint.Y - this.Top));
+
+            if (dragCursorPoint.Y - this.Top <= 50)
+            {
+                dragging = true;
+                dragFormPoint = this.Location;
+            }
+
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
+
+
+
