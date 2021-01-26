8d495845c5affc0db2e16a9b4c20a52c9bf2d301
HighwayToHeaven <alexmmych@gmail.com>
2021-01-26 19:13:36 +0100

    Now window is draggable thanks to the example given in https://github.com/qwqcode/CefSharpDraggableRegion.git

--- Form1.cs
+++ Form1.cs
@@ -9,12 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using CefSharp;
+using CefSharp.Enums;
 using CefSharp.WinForms;
 
 namespace Optimade
 {
     public partial class Optimade : Form
     {
+        // Messages
+        const int WM_MOUSEMOVE = 0x0200;
+        const int WM_MOUSELEAVE = 0x02A3;
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+
         public Optimade()
         {
             InitializeComponent();
@@ -22,11 +29,58 @@ namespace Optimade
 
             CefSharp cef = new CefSharp();
 
+            CefSharp.chromeBrowser.DragHandler = new DragDropHandler();
+
+            CefSharp.chromeBrowser.IsBrowserInitializedChanged += ChromeBrowser_IsBrowserInitializedChanged;
+            CefSharp.chromeBrowser.JavascriptMessageReceived += OnBrowserJavascriptMessageReceived;
+
             // Add it to the form and fill it to the form window.
             this.Controls.Add(CefSharp.chromeBrowser);
             CefSharp.chromeBrowser.Dock = DockStyle.Fill;
         }
 
+        private void ChromeBrowser_IsBrowserInitializedChanged(object sender, EventArgs args)
+        {
+
+
+            ChromeWidgetMessageInterceptor.SetupLoop(CefSharp.chromeBrowser, (message) =>
+            {
+                if (message.Msg == WM_LBUTTONDOWN)
+                {
+                    Point point = new Point(message.LParam.ToInt32());
+
+                    if (((DragDropHandler)CefSharp.chromeBrowser.DragHandler).draggableRegion.IsVisible(point))
+                    {
+                        ReleaseCapture();
+                        SendHandleMessage();
+                    }
+                }
+            });
+
+        }
+        private void OnBrowserJavascriptMessageReceived(object sender, JavascriptMessageReceivedEventArgs e)
+        {
+            if (e.Message.ToString() == "drag")
+            {
+                Console.WriteLine("Clicked");
+            }
+
+            if (e.Message.ToString() == "move")
+            { 
+                Console.WriteLine("Dragging");
+            }
+
+            if (e.Message.ToString() == "stop")
+            {
+                Console.WriteLine("Stop");
+            }
+             
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            Console.WriteLine("Mouse moved");
+        }
 
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         static extern IntPtr CreateRoundRectRgn
@@ -48,7 +102,24 @@ namespace Optimade
             Cef.Shutdown();
         }
 
-    }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public void SendHandleMessage()
+        {
+            if (InvokeRequired) { Invoke(new SendHandleMessageDelegate(SendHandleMessage), new object[] { }); return; }
+
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+        public delegate void SendHandleMessageDelegate();
+    
+}
+
 
     public class CefSharp
     {
@@ -72,7 +143,6 @@ namespace Optimade
             // Create a browser component
             chromeBrowser = new ChromiumWebBrowser(@"file:///C:/C%23/Optimade/Resources/Website/Website.html");
 
-            CefSharp.chromeBrowser.JavascriptMessageReceived += OnBrowserJavascriptMessageReceived;
             CefSharp.chromeBrowser.FrameLoadEnd += OnFrameLoadEnd;
         }
 
@@ -83,27 +153,37 @@ namespace Optimade
                 //In the main frame we inject some javascript that's run on mouseUp
                 //You can hook any javascript event you like.
                 CefSharp.chromeBrowser.ExecuteScriptAsync(@"
-                document.getElementById('caption').onmousedown = function () {
-                    CefSharp.PostMessage('drag');
+                var dragging = false;
+                var caption = document.getElementById('caption');
+                
+                function stopDrag()
+                {
+                    if (dragging == true) {
+                        dragging = false;
+                        CefSharp.PostMessage('stop');
+                    }
                 }
-                document.getElementById('caption').onmouseup = function () {
-                    CefSharp.PostMessage('stop');
-                }");
-            }
-        }
-        private void OnBrowserJavascriptMessageReceived(object sender, JavascriptMessageReceivedEventArgs e)
-        {
-            if (e.Message.ToString() == "drag")
-            {
-                Console.WriteLine("Dragging");
-            }
 
-            if (e.Message.ToString() == "stop")
-            {
-                Console.WriteLine("Stop");
+                caption.onmousedown = function () {
+                    dragging = true;
+                    CefSharp.PostMessage('drag');
+
+                    caption.addEventListener('mouseleave',stopDrag)
+                }
+
+                caption.onmousemove = function () {
+                        if (dragging == true) {
+                            CefSharp.PostMessage('move');
+                        }
+                }
+                caption.addEventListener('mouseup',stopDrag)");
             }
         }
+
+             
     }
+    
+
 }
 
 
